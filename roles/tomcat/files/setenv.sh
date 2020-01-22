c727544c33856af82de686933b936e7c36b3f6d3
pbalu-code <balazs.petik@pikkpakk.hu>
2020-01-22 14:30:31 +0100

    Add PostGIS support

--- roles/tomcat/files/setenv.sh
+++ roles/tomcat/files/setenv.sh
@@ -2,7 +2,6 @@ JAVA_HOME=/usr/java/default
 JAVA_OPTS="-Dfile.encoding=UTF-8 -Xms256m -Xmx1024m -XX:MaxPermSize=256M -server"
 LANG="en_US.UTF-8"
 
-#以下JMX監視用
 #CATALINA_OPTS="-Dcom.sun.management.jmxremote=true
 #               -Dcom.sun.management.jmxremote.port=10080
 #               -Dcom.sun.management.jmxremote.ssl=false
